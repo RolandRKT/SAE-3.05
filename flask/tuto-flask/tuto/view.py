6823eee596957204cf38aa2118fdf66dc098bfef
Rolaristos <roland.rakotomalala@etu.univ-orleans.fr>
2023-10-24 00:56:51 +0200

    Implémentation de la page home

---
+++ flask/tuto-flask/tuto/view.py
@@ -0,0 +1,15 @@
+from .app import app, db
+from flask import jsonify, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField ,HiddenField
+from wtforms.validators import DataRequired
+from flask import request
+from hashlib import sha256
+from wtforms import PasswordField
+from flask import request, redirect, url_for
+from wtforms import FloatField
+from flask import flash
+
+@app.route("/")
+def home():
+    return render_template("home.html")
