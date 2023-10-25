b808bb1327e5809ef3e152d4d52aabbec973486e
rebson <rdodjidhd@gmail.com>
2023-10-25 15:27:36 +0200

    Deux triggers (update et insert) sur parcours et une fonction; correction nom du dossier

--- Modele/creationtable.sql
+++ Modele/creationtable.sql
@@ -4,7 +4,8 @@ CREATE TABLE PARCOURS (
     date_debut DATETIME,
     date_fin DATETIME,
     description_parcours VARCHAR(200),
-    id_image int 
+    id_image int,
+    CHECK(date_debut < date_fin)
 )
 
 CREATE TABLE PARTICIPANT (
@@ -29,7 +30,6 @@ create table INTERETETAPE (
 create table ETAPE (
     id_etape int primary key,
     nom_etape varchar(200),
-    description_etape varchar(320),
     localisation varchar(200),
     id_image int 
 )
@@ -37,9 +37,10 @@ create table ETAPE (
 create table SUIVRE (
     id_participant int,
     id_parcours int,
-    note int,
+    note decimal(2,1),
     comm varchar(200),
-    primary key(id_parcours,id_participant)
+    primary key(id_parcours,id_participant),
+    CHECK(0 <= note and 5 >= note)
 )
 
 create table POSSEDER (
@@ -56,7 +57,11 @@ create table COMPOSER (
 
 -- ALTER TABLE pour attribuer les foreign key
 
+ALTER TABLE PARCOURS ADD UNIQUE (nom_parcours);
+
 ALTER TABLE PARCOURS ADD FOREIGN KEY (id_image) REFERENCES IMAGE(id_image);
+
+ALTER TABLE PARTICIPANT ADD UNIQUE (email);
 
 ALTER TABLE ETAPE ADD FOREIGN KEY (id_image) REFERENCES IMAGE(id_image);
 
@@ -71,4 +76,63 @@ ALTER TABLE POSSEDER ADD FOREIGN KEY (id_etape) REFERENCES ETAPE(id_etape);
 ALTER TABLE COMPOSER ADD FOREIGN KEY (id_parcours) REFERENCES PARCOURS(id_parcours);
 
 ALTER TABLE COMPOSER ADD FOREIGN KEY (id_etape) REFERENCES ETAPE(id_etape);
+
+-- Fonctions
+DELIMITER |
+CREATE FUNCTION CalculerDureeParcours(parcours_id INT) RETURNS TIME
+BEGIN
+    DECLARE debut DATETIME;
+    DECLARE fin DATETIME;
+    DECLARE duree TIME;
+
+    SELECT date_debut, date_fin INTO debut, fin
+    FROM PARCOURS
+    WHERE id_parcours = parcours_id;
+
+    SET duree = TIMEDIFF(fin, debut);
+
+    RETURN duree;
+END |
+DELIMITER ;
+
+
+-- Déclencheur pour empêcher que les parcours se chevauchent
+CREATE TRIGGER beforeInsertParcours BEFORE INSERT ON PARCOURS FOR EACH ROW
+BEGIN
+    DECLARE existing_count INT;
+    
+    -- Comptez les parcours existants dont la date de fin est après la nouvelle date de début
+    SET existing_count = (
+        SELECT COUNT(*)
+        FROM PARCOURS
+        WHERE new.date_debut > date_debut AND new.date_fin < date_fin
+    );
+    
+    -- Si des parcours se chevauchent, l'insertion est interdite
+    IF existing_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Les dates du parcours se chevauchent avec un parcours existant';
+    END IF;
+END;
+
+
+-- Déclencheur pour empêcher que les parcours se chevauchent
+CREATE TRIGGER beforeUpdateParcours BEFORE UPDATE ON PARCOURS FOR EACH ROW
+BEGIN
+    DECLARE existing_count INT;
+    
+    -- Comptez les parcours existants dont la date de fin est après la nouvelle date de début
+    SET existing_count = (
+        SELECT COUNT(*)
+        FROM PARCOURS
+        WHERE DATE(NEW.date_debut) = DATE(date_debut)
+        AND new.date_debut > date_debut AND new.date_fin < date_fin
+    );
+    
+    -- Si des parcours se chevauchent, l'insertion est interdite
+    IF existing_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Les dates du parcours se chevauchent avec un parcours existant';
+    END IF;
+END;
 
