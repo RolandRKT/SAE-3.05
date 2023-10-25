94187b0cd2d9bf051ce6aea7084d204479ed6dd0
Rolaristos <roland.rakotomalala@etu.univ-orleans.fr>
2023-10-25 12:28:55 +0200

    Ajout de la responsivité sur la page home

--- flask/tuto-flask/tuto/view.py
+++ flask/tuto-flask/tuto/view.py
@@ -1,7 +1,7 @@
 from .app import app, db
 from flask import jsonify, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField ,HiddenField
+from wtforms import StringField, HiddenField
 from wtforms.validators import DataRequired
 from flask import request
 from hashlib import sha256
@@ -9,7 +9,6 @@ from wtforms import PasswordField
 from flask import request, redirect, url_for
 from wtforms import FloatField
 from flask import flash
-
 
 @app.route("/")
 def home():
@@ -22,3 +21,20 @@ def login():
 @app.route("/create-account")
 def createaccount():
     return render_template("inscription.html")
+
+@app.route("/home_mobile")
+def home_mobile():
+    return render_template("home_mobile.html")
+
+@app.route("/login_mobile")
+def login_mobile():
+    return render_template("login_mobile.html")
+
+@app.route("/inscription_mobile")
+def inscription_mobile():
+    return render_template("inscription_mobile.html")
+
+# Nouvelle route pour effectuer la redirection
+@app.route("/redirect_home_mobile")
+def redirect_home_mobile():
+    return redirect(url_for("home_mobile"))
