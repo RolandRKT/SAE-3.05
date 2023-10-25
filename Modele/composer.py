3d056abe88de43bbf8cb63521f28500eb5f3bad4
Zenido <thibault.saint-leger@etu.univ-orleans.fr>
2023-10-25 10:15:30 +0200

    exploit: ajout get dans composer

--- Modele/composer.py
+++ Modele/composer.py
@@ -7,3 +7,8 @@ class Composer(db.Model):
     id_etape = db.Column(db.Integer, db.ForeignKey("etape.id_etape"), primary_key=True)
 
 
+def get_composer_parc_id(id_parc):
+    return get_parcours_id(id_parc)
+
+def get_composer_etape_id(id_etape):
+    return get_etape_id(id_etape)
