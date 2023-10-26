32aadb35fd5987ae85d1ccb23f6fb9e03b4d935f
Rolaristos <roland.rakotomalala@etu.univ-orleans.fr>
2023-10-26 13:26:48 +0200

    Fix : Responsivité - Correction pour samsung s10+ - Correction pour les pages pc - Correction de la réduction de page

--- flask/tuto-flask/tuto/static/script/apparition_logo.js
+++ flask/tuto-flask/tuto/static/script/apparition_logo.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const animatedImage1 = document.getElementById('animation-button1');
     const imgContent = document.getElementById('animation-button');
-    animatedImage1.style.transform = 'translateX(-8vw) scaleX(-1)';
+    animatedImage1.style.transform = 'translateX(-23vw) scaleX(-1)';
     imgContent.classList.add('hidden');
     setTimeout(function(){
         imgContent.classList.remove('hidden');
