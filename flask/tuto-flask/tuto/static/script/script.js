83bd9e130f7948bb3d1e3405c095a241ff4faa45
Rolaristos <roland.rakotomalala@etu.univ-orleans.fr>
2023-11-07 13:48:54 +0100

    Ajout de la class transition pour remplacer le querySelectorAll(a)

--- flask/tuto-flask/tuto/static/script/script.js
+++ flask/tuto-flask/tuto/static/script/script.js
@@ -1,21 +1,26 @@
-// Récupérez tous les liens de la page
-const links = document.querySelectorAll('a');
-const animatedImage = document.getElementById('animation-button');
-const animatedImage1 = document.getElementById('animation-button1');
+document.addEventListener('DOMContentLoaded', function() {
+  // Récupérez tous les liens de la page
+  const links = document.querySelectorAll('.transition');
+  const animatedImage = document.getElementById('animation-button');
+  const animatedImage1 = document.getElementById('animation-button1');
 
-// Fonction pour gérer le clic sur n'importe quel lien
-function handleLinkClick(event) {
-  event.preventDefault(); // Empêche le lien de déclencher la navigation par défaut
+  // Fonction pour gérer le clic sur n'importe quel lien
+  function handleLinkClick(event) {
+    event.preventDefault(); // Empêche le lien de déclencher la navigation par défaut
 
-  animatedImage.style.transform = 'translateX(100vw)'; // Déclenche l'animation
+    animatedImage.style.transform = 'translateX(100vw)'; // Déclenche l'animation
 
-  // Mettez un délai de 0.5s avant de naviguer vers le lien
-  setTimeout(function () {
-    window.location = event.target.href; // Navigue vers le lien
-  }, 600); // 0.5 seconde de délai
-}
+    // Mettez un délai de 0.5s avant de naviguer vers le lien
+    setTimeout(function () {
+      window.location = event.target.href; // Navigue vers le lien
+    }, 600); // 0.5 seconde de délai
+  }
 
-// Ajoutez le gestionnaire d'événements à tous les liens
-links.forEach(link => {
-  link.addEventListener('click', handleLinkClick);
+  // Gestionnaire d'événements à tous les liens
+  links.forEach(link => {
+    link.addEventListener('click', handleLinkClick);
+  });
 });
+
+
+
