a4e0c067ded4fe480486b88f56ebc5c722bf6a95
MaverickPrevost <maverickprevost@gmail.com>
2023-12-18 16:45:32 +0100

    feat: Ajout de la page mobile des portails

--- flask/tuto-flask/tuto/view.py
+++ flask/tuto-flask/tuto/view.py
@@ -46,7 +46,11 @@ def portails():
     administrateur.set_pseudo("")
     administrateur.set_mdp("")
     administrateur.set_id(-1)
-    return render_template("portails.html")
+    user_agent = request.user_agent.string
+    if any(keyword in user_agent for keyword in ["Mobi", "Android", "iPhone", "iPad"]):
+        return render_template("portail_mobile.html")
+    else:
+        return render_template("portails.html")
 
 @app.route("/login", methods=['GET','POST'])
 def login():
@@ -179,7 +183,7 @@ def connecter():
                     monimage=images[0].get_img_filename()
                     lesparcs.append((parc,monimage))
                 return redirect(url_for("accueil_admin"))
-    close_cnx()
+
     return redirect(url_for("login"))
 
 
@@ -208,7 +212,7 @@ def login_admin():
     print("hahaha")
     user_agent = request.user_agent.string
     if any(keyword in user_agent for keyword in ["Mobi", "Android", "iPhone", "iPad"]):
-        return render_template("login_admin.html", page_mobile = True)
+        return render_template("login_admin_mobile.html", page_mobile = True)
     else:
         return render_template("login_admin.html", page_mobile = False)
 
