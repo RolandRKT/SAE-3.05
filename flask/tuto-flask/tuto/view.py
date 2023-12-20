6a915a16c380c93d616be52d991a737e2f429289
RolandRKT <roland.rakotomalala@etu.univ-orleans.fr>
2023-12-20 09:23:14 +0100

    Finalisation de la page

--- flask/tuto-flask/tuto/view.py
+++ flask/tuto-flask/tuto/view.py
@@ -21,6 +21,10 @@ ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), './')
 sys.path.append(os.path.join(ROOT, './'))
 from app import mail
 
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), './')
+sys.path.append(os.path.join(ROOT, './'))
+from message import msg_forget_password, msg_inscription
+
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), './')
 sys.path.append(os.path.join(ROOT, 'modele/code_model/'))
@@ -307,48 +311,10 @@ def inscrire():
         inserer_le_participant(username, email, password)
         le_participant.set_all(PARTICIPANT.get_prochain_id_participant() - 1,
                                username, email, password)
-        #À TESTER
-        message_reset_password = f"""
-        <body style="width:100%; height:100%; color:black;">
-            <div class="page" style="position: relative; box-sizing: border-box; max-width:500px; font-family: cursive; font-size: 20px; border-radius: 10px; background: #fff; background-image: linear-gradient(#f5f5f0 1.1rem, #ccc 1.2rem); background-size: 100% 1.2rem; line-height: 1.2rem; padding: 1.4rem 1.5rem 0.2rem 1.5rem;">
-                <div style="display: flex; width: 100%; justify-content: center">
-                    <img src="https://raw.githubusercontent.com/RolandRKT/SAE-3.05/develop/flask/tuto-flask/tuto/static/images/wade-title.png" alt="Image" style="max-width: auto; height: 70px; display: block; margin-left:auto; margin-right:auto;">
-                </div>
-                <div class="margin"></div>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Bienvenue {username} chez Wade !
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Nous sommes ravis de vous accueillir parmi nous.
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Votre compte a été créé avec succès.
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700" style="background-color:white; text-align:center; font-weight:bold; height:25px; font-size:20px;">
-                    Votre mot de passe : {password}
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Connectez-vous à votre compte en utilisant votre adresse e-mail et le mot de passe fourni.
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Merci de faire partie de notre communauté.
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    Cordialement,
-                </p>
-                <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                    L'équipe de Wade
-                </p>
-                <div style="display: flex; width: 100%; justify-content: center">
-                    <img src="https://raw.githubusercontent.com/RolandRKT/SAE-3.05/develop/flask/tuto-flask/tuto/static/images/logo.png" alt="Image" style="max-width: 70px; height: auto; display: block; margin-left:auto; margin-right:auto;">
-                </div>
-            </div>
-        </body>
-        """
         msg = Message("✨Bienvenue chez Wade !✨",
                       recipients=[email])
         msg.body = "Cher utilisateur..."
-        msg.html = message_reset_password
+        msg.html = msg_inscription(username, password)
         mail.send(msg)
         return jsonify({"success": "registered"})
 
@@ -535,57 +501,15 @@ def suppression_participant(pseudo):
 def forget_password():
     if request.method == 'POST':
         email = request.form.get('email')
-        # password = PARTICIPANT.get_par_mail_mdp(email)
-        password = "SuperStrngon"
-        
+        password = PARTICIPANT.get_par_mail_mdp(email)
         if password is not None:
-            message_reset_password = """
-            <body style="width:100%; height:100%; color:black;">
-                <div class="page" style="position: relative; box-sizing: border-box; max-width:500px; font-family: cursive; font-size: 20px; border-radius: 10px; background: #fff; background-image: linear-gradient(#f5f5f0 1.1rem, #ccc 1.2rem); background-size: 100% 1.2rem; line-height: 1.2rem; padding: 1.4rem 1.5rem 0.2rem 1.5rem;">
-                    <div style="display: flex; width: 100%; justify-content: center">
-                        <img src="https://raw.githubusercontent.com/RolandRKT/SAE-3.05/develop/flask/tuto-flask/tuto/static/images/wade-title.png" alt="Image" style="max-width: auto; height: 70px; display: block; margin-left:auto; margin-right:auto;">
-                    </div>
-                    <div class="margin"></div>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Cher utilisateur,
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Nous avons reçu une demande de réinitialisation du mot de passe associé à cette adresse e-mail. Si vous n'avez pas fait cette demande, veuillez ignorer cet e-mail.
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Pour rappel, votre mot de passe est :
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700" style="background-color:white; text-align:center; font-weight:bold; height:25px; font-size:20px;">
-                        {}
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Veuillez vous connecter à votre compte avec ce mot de passe.
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Merci de faire partie de notre communauté.
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        Cordialement,
-                    </p>
-                    <p class="px-10 text-[10px] sm:text-[12px] text-gray-700">
-                        L'équipe de Wade
-                    </p>
-                    <div style="display: flex; width: 100%; justify-content: center">
-                        <img src="https://raw.githubusercontent.com/RolandRKT/SAE-3.05/develop/flask/tuto-flask/tuto/static/images/logo.png" alt="Image" style="max-width: 70px; height: auto; display: block; margin-left:auto; margin-right:auto;">
-                    </div>
-                </div>
-            </body>
-            """.format(password)
 
             msg = Message("Wade - Mot de passe oublié ?",
-                          recipients=['nekokami022@gmail.com'])
+                          recipients=[email])
             msg.body = "Cher utilisateur..."
-            msg.html = message_reset_password
+            msg.html = msg_forget_password(password)
             mail.send(msg)
             # Ajouter une redirection vers une page qui dit envoie validé, ou juste une popup
             return render_template("forget.password.html")
 
     return render_template("forget.password.html")
-
-#test123wade@gmail.com
-#Baba45ls!
